21489e10f4d63fa35a5f4714cc24669697535c19
Oliver Molnar <oliver.molnar@420cloud.io>
2025-09-12 12:26:07 +0200

    setup bun full stack

---
+++ src/server/frontend.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { createRoot } from "react-dom/client";
+import { App } from "./app.tsx";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const container = document.getElementById("root");
+
+	if (!container) {
+		throw new Error("Root container missing in index.html");
+	}
+
+	const root = createRoot(container);
+	root.render(<App />);
+});
